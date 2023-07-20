f1e31a4a64fc5448fa5a42d3ff8cf15de760e07a
Rich Schumacher <rich.schumacher@bazaarvoice.com>
2023-07-20 10:36:33 -0400

    feat: Add nvm support

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Output all possible results for make targets
+zstyle ':completion:*:make:*:targets' call-command true
+
+zstyle ':completion:*:*:make:*' tag-order 'targets'
