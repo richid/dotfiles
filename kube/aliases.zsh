94347b6a0acc1772903ef1e2fbd2f4ab3a092c13
Rich Schumacher <rich.schumacher@brivo.com>
2024-12-17 13:54:55 -0500

    feat: Add kube config

---
+++ kube/aliases.zsh
@@ -0,0 +1,5 @@
+alias k="kubectl "
+alias kx="kube-exec "
+alias kl="kube-logs "
+
+source <(kubectl completion zsh)
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -28,6 +28,4 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+unsetopt completealiases # https://unix.stackexchange.com/questions/335145/using-zsh-autocompletion-for-alias
