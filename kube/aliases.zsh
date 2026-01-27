12e36bfcc599b2f64356cfae77487a05ce5b9f5c
Rich Schumacher <rich.schu@gmail.com>
2026-01-26 21:29:06 -0500

    fix: Check for kubectl and direnv before using

--- kube/aliases.zsh
+++ kube/aliases.zsh
@@ -2,4 +2,6 @@ alias k="kubectl "
 alias kx="kube-exec "
 alias kl="kube-logs "
 
-source <(kubectl completion zsh)
+if command -v kubectl &> /dev/null; then
+    source <(kubectl completion zsh)
+fi
